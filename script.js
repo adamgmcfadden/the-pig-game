96efc753aff856910ae1f9aaabd7324601433b51
Adam Girard-McFadden <adam_m_20@hotmail.com>
2022-01-27 10:57:05 -0500

    starting conditions of game complete

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// save required elements as variables
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
